8835bab174b93e1c8b4660941ec92757ec4d327d
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2022-11-22 19:44:29 +0700

    Add files via upload

---
+++ main/peng.sh
@@ -0,0 +1,14 @@
+clear
+echo "##############################"
+echo "         PENGURANGAN          "
+echo "##############################"
+echo ""
+read -p "Masukan jumlah ke 1: " jumOne
+read -p "Masukan jumlah ke 2: " jumTwo
+
+let val1=$jumOne-$jumTwo
+ echo "Hasil dari" $jumOne "-" $jumTwo = $val1
+ sleep 4
+ cd
+ cd kalkulator
+ bash main.sh
